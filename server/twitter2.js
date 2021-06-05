3618313f145c8840997e21c21a6e24b588f7edaa
Siebe Konst <konstsiebe@hotmail.com>
2021-06-05 10:27:28 +0200

    added date field

--- server/twitter2.js
+++ server/twitter2.js
@@ -17,7 +17,7 @@ const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
 
 
 
-async function getTweets(params,coin) {
+async function getTweets(params) {
 
     const res = await needle('get', endpointUrl, params, {
         headers: {
@@ -38,29 +38,35 @@ async function getTweets(params,coin) {
     try {
 
         const params = {
-            'query': '#ADA',
-            'tweet.fields': 'text',
+            'query': `#BTC`,
+            'tweet.fields': 'text,created_at', // add date here
             'max_results': 10,
-
         }
 
                     
         const coins = await current_coin_prices(null, false)
         let tickers = Object.keys(coins)
+
+        // Add list of coins and count of tweets per day, look for significant rises
         
         
         for (i = 0; i <= tickers.length; i++) {
 
             console.log(` \x1b[31m Getting #${tickers[i]} \x1b[0m`)
             while (true) {
-        
-
-                let response = await getTweets(params,tickers[i]);
+                params['query'] = `#${tickers[i]}`;
+                console.log(params['query'])
+                
+                let response = await getTweets(params);
+                
                 params['next_token'] = response["meta"]["next_token"]
-                console.log(response)
-
+                console.log(response['data'][1]['text']);
+                
                 if (!(params['next_token'])) {
-                    console.log("No more tweets to get")
+                    console.log(`No more tweets to get for ${tickers[i]} \n Waiting 20 seconds`)
+                    await new Promise(r => setTimeout(r, 20000));
+                    //console.log(`Amount of tweets for #${tickers[i]}: ${response.length}`)
+
                 }
             }
         }
